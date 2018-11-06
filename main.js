ff2d8848cf5927455f65ff75d5d5da15b99d0e24
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-06 22:30:54 +0800

    Added base files

---
+++ main.js
@@ -0,0 +1,50 @@
+var app = new Vue({
+    el: ".app",
+    data() {
+        return {
+            number: "",
+            mode: 'plus',
+            previous: "",
+            answer: "",
+        }
+    },
+
+    methods: {
+        concat(no) {
+            this.number = this.number + no;
+        },
+
+        setMode(mode) {
+            this.previous = this.number;
+            this.mode = mode;
+            this.number = ""
+        },
+
+        evaluate() {
+
+            console.log(Number(this.number))
+            console.log(Number(this.previous))
+
+            console.log(this.mode)
+            
+            if (this.mode == 'plus') {
+                console.log('plus')
+                this.answer = Number(this.number) + Number(this.previous);
+                console.log(this.answer)
+            } else if (this.mode == 'minus') {
+                console.log('minus')
+                this.answer = Number(this.number) - Number(this.previous);
+                console.log(this.answer)
+            } else if (this.mode == 'times') {
+                console.log('times')
+                this.answer = Number(this.number) * Number(this.previous);
+                console.log(this.answer)
+            } else if (this.mode == 'divide') {
+                console.log('divide')
+                this.answer = Number(this.number) / Number(this.previous);
+                console.log(this.answer)
+            }
+
+        }
+    }
+})
